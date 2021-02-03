3f933b9f9d7bdf5c238505a7a3ed4d8b5bd10c3a
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-02-02 14:54:19 -1000

    Update main.py
    Adds first functions

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+#!/bin/env python
+from api import NU
 #make loop
 run = True
 
 #start loop
 while run:
+    #get input
+    inp = input(">>")
+    
+    #check to see if inp is the same as a string
+    if inp == "hello":
+        print("Hi, how are you today?")
+        feeling = input(">>")
+        if feeling == "good" or feeling == "fine":
+            print ("Thats good")
+        if feeling == "bad" or feeling == "not so good" or feeling == "very bad":
+            print("Thats not so good")
+    else:
+        if inp == "what can you do":
+            print("Answer your hellos(more functions to come)")
+        else:
+            if inp == "stop":
+                print("Stop in progress")
+                run = False
+            else:
+                NU()
