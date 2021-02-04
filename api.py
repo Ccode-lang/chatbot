75bb514b78797c51cebfd286e01af21f0b5de878
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-02-04 07:26:14 -1000

    new Release

--- api.py
+++ api.py
@@ -6,3 +6,9 @@ def NU():
 def add(a, b):
     total = a + b
     return total
+
+
+def ver():
+    print("Python Chatbot (b 1.0.1f).")
+    print("Type 'what can you do' to see what this version can do.")
+    print("This is a beta version.")
