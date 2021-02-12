5e1d13f6e089c2affcad81a298de70cec7e46385
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-02-12 13:17:45 -1000

    Update api.py

--- api.py
+++ api.py
@@ -12,3 +12,10 @@ def ver():
     print("Python Chatbot (b 1.0.2 in dev).")
     print("Type 'what can you do' to see what this version can do.")
     print("This is a dev version")
+
+def list():
+    print("hi or hello(program will ask how you are, answer and it will give an answer)")
+    print("what can you do (shows this)")
+    print("stop(stops program)")
+    print("add(add to numbers)")
+    print("thanks(says 'your welcome')")
