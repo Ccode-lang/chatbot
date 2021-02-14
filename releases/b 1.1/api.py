75f1776ee2a88ef945d3065d3672ef1fe1a9cdee
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-02-13 18:37:11 -0700

    Create api.py

---
+++ releases/b 1.1/api.py
@@ -0,0 +1,21 @@
+
+
+def NU():
+    print("I do not understand")
+
+def add(a, b):
+    total = a + b
+    return total
+
+
+def ver():
+    print("Python Chatbot (b 1.1 in dev).")
+    print("Type 'what can you do' to see what this version can do.")
+    print("This is a dev version")
+
+def list():
+    print("hi or hello(program will ask how you are, answer and it will give an answer)")
+    print("what can you do (shows this)")
+    print("stop(stops program)")
+    print("add(add to numbers)")
+    print("thanks(says 'your welcome')")
