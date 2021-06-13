5359ff2effd7c2aeb8c27234639b5f49e10659f2
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-06-13 11:56:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
+def NU():
+    print("I do not understand")
+
+def add(a, b):
+    total = a + b
+    return total
+
+
+def ver():
+    print("Python Chatbot (1.0).")
+    print("Type 'what can you do' to see what this version can do.")
+    print("")
+
+def list():
+    print("hi or hello(program will ask how you are, answer and it will give an answer)")
+    print("what can you do (shows this)")
+    print("stop or quit(stops program)")
+    print("add(add to numbers)")
+    print("thanks(says 'your welcome')")
+    print("how are you today(answers)")
+    print("what are you(says purpose)")
+
+
+
+
+
+
+
+
+
+
+
+
 #make loop
+
+
 run = True
 
 #make compatable with py2 and 3
